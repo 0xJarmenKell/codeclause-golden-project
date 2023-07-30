4d9c8554431c4b37348996ca4f8d3ea713e18ab4
Youssef Y. Younis <87781261+YoussefYousry994@users.noreply.github.com>
2023-07-30 04:53:17 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ function App() {
 
   const fileInputRef = useRef();
 
-  const url = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
 
   useEffect(() => {
     const getImage = async () => {
@@ -30,7 +29,6 @@ function App() {
 
   return (
     <div className='container'>
-      {/* <img src={url} className='img' /> */}
       <div className='wrapper'>
         <h1>Share <span className='logo'>Me</span></h1>
         <p>Upload and share the download link.</p>
